4725d7513ada17cf2fffc021cf2017089bd9443b
Suwen macOS Yurisa <577014284@qq.com>
2020-04-30 18:10:24 +0800

    引入MDB，nz。

--- src/main/webapp/easy-springboot-web/src/app/app.module.ts
+++ src/main/webapp/easy-springboot-web/src/app/app.module.ts
@@ -8,6 +8,18 @@ import { SideNavComponent } from './side-nav/side-nav.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { Code404Component } from './code404/code404.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -21,9 +33,15 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot()
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main/webapp/easy-springboot-web/src/app/side-nav/side-nav.component.ts
+++ src/main/webapp/easy-springboot-web/src/app/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-  constructor() { }
+  toggleFlag = true;
+  userName = 'test user';
+
+  onToggle() {
+    this.toggleFlag = (this.toggleFlag !== true);
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  logout() {
+  }
+
 }
+
+
